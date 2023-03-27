f7296a31e83fe26240137404b56e64b9997afe04
Jengamon <bob.hostern@gmail.com>
2023-03-27 13:41:31 -0700

    Update build.zig
    expose module

--- build.zig
+++ build.zig
@@ -1,26 +1,39 @@
-const Builder = @import("std").build.Builder;
+const Build = @import("std").Build;
+const FileSource = Build.FileSource;
 
-pub fn build(b: *Builder) void {
+pub fn build(b: *Build) void {
     const target = b.standardTargetOptions(.{});
-    const mode = b.standardReleaseOptions();
+    const optimize = b.standardOptimizeOption(.{});
 
-    const lib = b.addStaticLibrary("zig-wcwidth", "src/main.zig");
-    lib.setTarget(target);
-    lib.setBuildMode(mode);
+    _ = b.addModule("wcwidth", .{
+        .source_file = .{ .path = "src/main.zig" },
+    });
+
+    const lib = b.addStaticLibrary(.{
+        .name = "zig-wcwidth",
+        .root_source_file = FileSource.relative("src/main.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
     lib.install();
 
-    var generate = b.addExecutable("generate", "tools/generate.zig");
-    generate.setTarget(target);
-    generate.setBuildMode(mode);
+    var generate = b.addExecutable(.{
+        .name = "generate",
+        .root_source_file = FileSource.relative("tools/generate.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
 
     var generate_run = generate.run();
 
     const generate_step = b.step("generate", "Generate tables");
     generate_step.dependOn(&generate_run.step);
 
-    var main_tests = b.addTest("src/test.zig");
-    main_tests.setTarget(target);
-    main_tests.setBuildMode(mode);
+    var main_tests = b.addTest(.{
+        .root_source_file = FileSource.relative("src/test.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
 
     const test_step = b.step("test", "Run library tests");
     test_step.dependOn(&main_tests.step);
